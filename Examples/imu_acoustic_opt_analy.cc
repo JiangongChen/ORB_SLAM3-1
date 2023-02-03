8a1a2b0825750c265639de317305c9ab0361af4f
Jiangong Chen <chenjg1998@gmail.com>
2023-02-03 11:40:49 -0500

    Adapt ORB-SLAM preintegration

--- Examples/imu_acoustic_opt_analy.cc
+++ Examples/imu_acoustic_opt_analy.cc
@@ -4,16 +4,16 @@
 using std::cout;
 using std::endl; 
 void readDistances(const string & filename, vector<double>& ts, vector<double>& distances);
-void readTrajTS(const string& filename, vector<double>& tss);
+void readTraj(const string& filename, vector<double>& tss, vector<g2o::SE3Quat> &traj);
 void readIMU(const string& filename, vector<double>& tss, vector<Eigen::Vector3d>& acces, vector<Eigen::Vector3d>& gyros);
 void writeData(const string& filename, vector<double>& tss, vector<Eigen::Vector3d>& pos);
 
 // perform offline analysis for the optimization of acoustic ranging and imu tracking
 int main(int argc, char **argv)
 {
-    if(argc != 5)
+    if(argc != 6)
     {
-        cerr << endl << "Usage: ./opt_analy path_to_distances_measurements path_to_traj_file path_to_imu_file path_to_store_output" << endl;
+        cerr << endl << "Usage: ./opt_analy path_to_distances_measurements path_to_traj_file path_to_imu_file path_to_store_output smartphone_setup" << endl;
         return 1;
     }
 /*scale: 1.116997
@@ -37,7 +37,8 @@ tran:
     vector<double> dist_ts; 
     readDistances(argv[1], dist_ts, all_distances);
     vector<double> ts_traj;
-    readTrajTS(argv[2],ts_traj);
+    vector<g2o::SE3Quat> slam_traj;
+    readTraj(argv[2],ts_traj,slam_traj);
     //cout << setprecision(12) << ts_traj[0] << " " << ts_traj[1] << endl; 
     vector<vector<double>> round_distances;
     for(int i=0;i<all_distances.size()/4;i++){
@@ -51,12 +52,12 @@ tran:
     }
     vector<Eigen::Vector3d> imu_acce;
     vector<Eigen::Vector3d> imu_gyro;
-    vector<double> imu_ts;
-    readIMU(argv[3],imu_ts,imu_acce,imu_gyro);
+    vector<double> ts_imu;
+    readIMU(argv[3],ts_imu,imu_acce,imu_gyro);
     // test whether IMU has been successfully extracted
     //for(int i=0;i<3;i++){
-        //cout << imu_ts[i] << " " << imu_acce[i].transpose() << " " << imu_gyro[i].transpose() << endl;  
-        //cout << imu_ts[i] << " " << imu_acce[i][0] << " " << imu_acce[i][1] << " " << imu_acce[i][2] << " " << 
+        //cout << ts_imu[i] << " " << imu_acce[i].transpose() << " " << imu_gyro[i].transpose() << endl;  
+        //cout << ts_imu[i] << " " << imu_acce[i][0] << " " << imu_acce[i][1] << " " << imu_acce[i][2] << " " << 
         //    imu_gyro[i][0] << " " << imu_gyro[i][1] << " " << imu_gyro[i][2] << endl;  
     //}
 
@@ -73,30 +74,96 @@ tran:
     vector<Eigen::Vector3d> imu_only_pose;
     vector<double> cal_ts;
     int ac_cnt = 0;
-    for (size_t i=0;i<imu_ts.size()-1;i++){
-        if (imu_ts[i]<init_time) continue; 
-        if (imu_ts[i]>end_time) break;
-        Eigen::Vector3d g_b = rot.transpose()*g_w - rot.transpose()*pos;
-        velocity = velocity + (imu_acce[i]-g_b)*(imu_ts[i+1]-imu_ts[i]);
-        Eigen::Vector3d delta = velocity*(imu_ts[i+1]-imu_ts[i]);
-        pos = rot*delta + pos; 
-        Eigen::Vector3d delta_ang = imu_gyro[i]*(imu_ts[i+1]-imu_ts[i]);
-        Eigen::Quaterniond delta_quat = Eigen::AngleAxisd(delta_ang[0], Eigen::Vector3d::UnitX())
-                                * Eigen::AngleAxisd(delta_ang[1], Eigen::Vector3d::UnitY())
-                                * Eigen::AngleAxisd(delta_ang[2], Eigen::Vector3d::UnitZ());
-        rot = rot*delta_quat.toRotationMatrix();
-        // reset imu using acoustic 
-        if (ac_cnt < dist_ts.size() && imu_ts[i] > dist_ts[ac_cnt*4]){
-            Eigen::Vector3d est;
-            //ORB_SLAM3::Optimizer::PoseOptimizationDistanceGivenScale(est,1.116997,other_trans,round_distances[ac_cnt]); 
-            velocity = Eigen::Vector3d(); 
-            pos = est;
-            ac_cnt++;
-            //cout << "time: "<<dist_ts[ac_cnt*4] << "estimated pose: "<<pos.transpose() << endl; 
+    int i = 0;
+    while (i<ts_imu.size()&&ts_imu[i]<ts_traj[0]) i++; 
+    //cout << i << " " << ts_imu[i] << endl;
+
+    // load imu parameters
+    ORB_SLAM3::Settings* settings = new ORB_SLAM3::Settings(argv[5],ORB_SLAM3::System::eSensor::IMU_MONOCULAR);
+    ORB_SLAM3::IMU::Calib* mImuCalib;
+    // parse imu calibration
+    Sophus::SE3f Tbc = settings->Tbc();
+    double mImuFreq = settings->imuFrequency();
+    float Ng = settings->noiseGyro();
+    float Na = settings->noiseAcc();
+    float Ngw = settings->gyroWalk();
+    float Naw = settings->accWalk();
+
+    const float sf = sqrt(mImuFreq);
+    mImuCalib = new ORB_SLAM3::IMU::Calib(Tbc,Ng*sf,Na*sf,Ngw/sf,Naw/sf);
+    //cout<<mImuCalib->mTcb.translation() << endl; 
+    ORB_SLAM3::IMU::Bias mBias = ORB_SLAM3::IMU::Bias(); 
+
+    for (size_t id_traj=1;id_traj<ts_traj.size();id_traj++){
+        vector<ORB_SLAM3::IMU::Point> mvImuFromLastFrame;
+        while (i<ts_imu.size()&&ts_imu[i]<ts_traj[id_traj]) {
+            ORB_SLAM3::IMU::Point p(imu_acce[i][0],imu_acce[i][1],imu_acce[i][2],imu_gyro[i][0],imu_gyro[i][1],imu_gyro[i][2],ts_imu[i]);
+            mvImuFromLastFrame.push_back(p);
+            // self defined double integration
+            /*Eigen::Vector3d g_b = rot.transpose()*g_w - rot.transpose()*pos;
+            velocity = velocity + (imu_acce[i]-g_b)*(ts_imu[i+1]-ts_imu[i]);
+            Eigen::Vector3d delta = velocity*(ts_imu[i+1]-ts_imu[i]);
+            pos = rot*delta + pos; 
+            Eigen::Vector3d delta_ang = imu_gyro[i]*(ts_imu[i+1]-ts_imu[i]);
+            Eigen::Quaterniond delta_quat = Eigen::AngleAxisd(delta_ang[0], Eigen::Vector3d::UnitX())
+                                    * Eigen::AngleAxisd(delta_ang[1], Eigen::Vector3d::UnitY())
+                                    * Eigen::AngleAxisd(delta_ang[2], Eigen::Vector3d::UnitZ());
+            rot = rot*delta_quat.toRotationMatrix();*/
+            i++; 
         }
-        cal_ts.push_back(imu_ts[i]);
+        //cout << i << " " << mvImuFromLastFrame.size() << endl; 
+        // use ORB SLAM method
+        ORB_SLAM3::IMU::Preintegrated* pImuPreintegratedFromLastFrame = new ORB_SLAM3::IMU::Preintegrated(mBias,*mImuCalib);
+        for(int j=0;j<mvImuFromLastFrame.size()-1;j++){
+            Eigen::Vector3f acc = (mvImuFromLastFrame[j].a+mvImuFromLastFrame[j+1].a)*0.5f;
+            Eigen::Vector3f angVel = (mvImuFromLastFrame[j].w+mvImuFromLastFrame[j+1].w)*0.5f;
+            float tstep = mvImuFromLastFrame[j+1].t-mvImuFromLastFrame[j].t;
+            pImuPreintegratedFromLastFrame->IntegrateNewMeasurement(acc,angVel,tstep);
+        }
+        const Eigen::Vector3f twb1 = pos.cast<float>();
+        const Eigen::Matrix3f Rwb1 = rot.cast<float>();
+        const Eigen::Vector3f Vwb1 = velocity.cast<float>();
+        const Eigen::Vector3f Gz(0, 0, -ORB_SLAM3::IMU::GRAVITY_VALUE);
+        const float t12 = ts_traj[id_traj] - ts_traj[id_traj-1];
+
+        Eigen::Matrix3f Rwb2 = ORB_SLAM3::IMU::NormalizeRotation(Rwb1 * pImuPreintegratedFromLastFrame->GetDeltaRotation(mBias));
+        Eigen::Vector3f twb2 = twb1 + Vwb1*t12 + 0.5f*t12*t12*Gz+ Rwb1 * pImuPreintegratedFromLastFrame->GetDeltaPosition(mBias);
+        Eigen::Vector3f Vwb2 = Vwb1 + t12*Gz + Rwb1 * pImuPreintegratedFromLastFrame->GetDeltaVelocity(mBias);
+
+        
+        // check static case
+        double ave_acc_mag = 0;
+        for(int j=0;j<mvImuFromLastFrame.size();j++){
+            ave_acc_mag += mvImuFromLastFrame[j].a.norm();
+        }
+        ave_acc_mag /= mvImuFromLastFrame.size();
+        if (abs(ave_acc_mag-ORB_SLAM3::IMU::GRAVITY_VALUE)<0.1 ){
+            // pos, rot, not change, set velocity to 0
+            velocity = Eigen::Vector3d(0,0,0); 
+        }
+        else {
+            pos = twb2.cast<double>();
+            rot = Rwb2.cast<double>();
+            //rot = slam_traj[id_traj].rotation().toRotationMatrix();
+            velocity = Vwb2.cast<double>();
+        }
+
+        mBias = pImuPreintegratedFromLastFrame->b;
+
+        // reset imu using acoustic 
+        if (ac_cnt < dist_ts.size() && ts_imu[i] > dist_ts[ac_cnt*4]){
+            Eigen::Vector3d est;
+            ORB_SLAM3::Optimizer::PoseOptimizationDistanceGivenScale(est,1.116997,other_trans,round_distances[ac_cnt]); 
+            velocity = Eigen::Vector3d(0,0,0); 
+            pos = est;
+            //rot = slam_traj[id_traj].rotation().toRotationMatrix();
+            ac_cnt++;
+            //cout << "time: "<<dist_ts[ac_cnt*4] << "estimated pose: "<<pos.transpose() << "reset velocity" << velocity.transpose() << endl; 
+        }
+        cal_ts.push_back(ts_traj[id_traj]);
         imu_only_pose.push_back(pos);
     }
+    //cout << mBias.bax << " " << mBias.bay << " " << mBias.baz << endl; 
     writeData(argv[4],cal_ts,imu_only_pose);
     /*for(size_t i=0;i<round_distances.size();i++){
         ORB_SLAM3::Optimizer::PoseOptimizationDistanceGivenScale(est_trans,1.116997,other_trans,round_distances[i]); 
@@ -134,7 +201,7 @@ void readDistances(const string & filename, vector<double>& ts, vector<double>& 
     fin.close();
 }
 
-void readTrajTS(const string& filename, vector<double>& tss){
+void readTraj(const string& filename, vector<double>& tss, vector<g2o::SE3Quat> &traj){
     // read timestamps of the recorded trajectory 
     ifstream fin(filename);
     if (!fin) {
@@ -153,7 +220,10 @@ void readTrajTS(const string& filename, vector<double>& tss){
         fin >> trajectory_time >> tx >> ty >> tz >> qx >> qy >> qz >> qw >> mark;
         cnt++; 
         tss.push_back(atof(trajectory_time.c_str()));
-
+        Eigen::Vector3d pos(atof(tx.c_str()), atof(ty.c_str()), atof(tz.c_str()));
+        Eigen::Quaterniond q(atof(qw.c_str()), atof(qx.c_str()), atof(qy.c_str()), atof(qz.c_str()) );
+        g2o::SE3Quat pose(q,pos);
+        traj.push_back(pose);
     }
     
     fin.close();
@@ -213,3 +283,4 @@ void writeData(const string& filename, vector<double>& tss, vector<Eigen::Vector
     f.close();
     std::cout << endl << filename << " saved!" << endl;
 }
+
